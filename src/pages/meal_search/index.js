961ccdb67bc57e39570b062e4712d9cd6f99ae04
Gabriel Al-Rifai <Gabrielalrifai@gmail.com>
2024-03-25 14:02:19 -0500

    updated navbar

---
+++ src/pages/meal_search/index.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import { useAuth } from "../../hooks/auth";
+
+// const Meal_Search = () => {
+//   const { logout } = useAuth();
+
+//   const handleClick = () => {
+//     logout();
+//   };
+
+//   return (
+//     <div>
+//       <h1>You are logged in so can see this</h1>
+//       <button onClick={handleClick}>LogOut</button>
+//     </div>
+//   );
+// };
+
+// export default Meal_Search;
