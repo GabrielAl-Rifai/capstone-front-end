f2697590fec674c74250a27da2c6e64f558fa411
Gabriel Al-Rifai <Gabrielalrifai@gmail.com>
2024-03-24 22:11:32 -0500

    updated navbar index.js

---
+++ src/components/navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/auth">Login</Link>
+      <Link to="/dashboard">Dashboard</Link>
+    </nav>
+  );
+};
